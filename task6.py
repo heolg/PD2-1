31e1e38d9b895da906bcac8b82acf685f3bc4c7d
heolg <raubiskoartem@gmail.com>
2026-03-03 20:18:25 +1300

    Add files via upload

---
+++ task6.py
@@ -0,0 +1,31 @@
+# Hostname; IP; Latency(ms); Status
+nodes = [
+"Srv-Web-01;192.168.1.10;15;UP",
+"Srv-DB-01;192.168.1.20;450;UP",
+"Srv-Backup;10.0.0.5;0;DOWN",
+"Workstation-A;192.168.1.105;5;UP",
+"Srv-Proxy-01;172.16.0.1;10;up",
+"Srv-Mail;10.0.0.10;120;UP",
+"Router-Core;192.168.1.1;2;UP",
+"Srv-Dev-01;192.168.2.50;500;UP",
+"Printer-Main;192.168.1.200;0;down",
+"Srv-Log;10.0.0.15;105;UP"
+]
+
+def get_srv_status(nodes:)
+    result = {}
+
+    for node in nodes:
+        parts = node.split(";")
+        hostname = parts[0]
+        ip = parts[1]
+        status = parts[3].upper()
+
+        if hostname.startswitch("Srv"):
+            result[ip] = status
+        
+    return result
+
+
+srv_status = get_srv_status(nodes)
+print(srv_status)
